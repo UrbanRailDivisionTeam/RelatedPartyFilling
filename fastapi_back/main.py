f4f877069c5f1d79dcc539a3bd32e5c85fcad152
cheakf <sunmoming#gmail.com>
2025-03-12 21:20:28 +0800

    暂存后端关于数据库的建模

--- fastapi_back/main.py
+++ fastapi_back/main.py
@@ -1,7 +1,9 @@
 import uvicorn
+import queue
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from model import SafeFrom, database
+from peewee import JOIN
+from model import create_tables, SafeForm, SafeFormHead, workLocation, dangerTypes, accompaningPersons
 
 app = FastAPI(root_path="/api")
 origins = [
@@ -25,14 +27,82 @@ async def login_user(user_id: str):
 
 # 提交申请
 @app.post("/safety/submit")
-async def applications(safefrom: SafeFrom):
+async def applications(safefrom: SafeForm):
+    temp_head = SafeFormHead()
+    temp_head.applicationNumber = safefrom.applicationNumber
+    temp_head.name = safefrom.name
+    temp_head.idNumber = safefrom.idNumber
+    temp_head.companyName = safefrom.companyName
+    temp_head.phoneNumber = safefrom.phoneNumber
+    temp_head.submitTime = safefrom.submitTime
+    temp_head.startDate = safefrom.startDate
+    temp_head.startTime = safefrom.startTime
+    temp_head.workingHours = safefrom.workingHours
+    temp_head.workType = safefrom.workType
+    temp_head.isProductWork = safefrom.isProductWork
+    temp_head.projectName = safefrom.projectName
+    temp_head.vehicleNumber = safefrom.vehicleNumber
+    temp_head.trackPosition = safefrom.trackPosition
+    temp_head.workContent = safefrom.workContent
+    temp_head.workBasis = safefrom.workBasis
+    temp_head.basisNumber = safefrom.basisNumber
+    temp_head.isDangerousWork = safefrom.isDangerousWork
+    temp_head.notifierName = safefrom.notifierName
+    temp_head.notifierNumber = safefrom.notifierNumber
+    temp_head.notifierDepartment = safefrom.notifierDepartment
+    temp_head.accompaningCount = safefrom.accompaningCount
+    temp_head.insert()
+    
+    for ch in safefrom.workLocation:
+        temp_work = workLocation()
+        temp_work.formApplicationNumber = safefrom.applicationNumber
+        temp_work.workLocation = ch
+        temp_work.insert()
+        
+    for ch in safefrom.dangerTypes:
+        temp_danger = dangerTypes()
+        temp_danger.formApplicationNumber = safefrom.applicationNumber
+        temp_danger.dangerTypes = ch
+        temp_danger.insert()
+        
+    for ch in safefrom.accompaningPersons:
+        temp_person = accompaningPersons()
+        temp_person.formApplicationNumber = safefrom.applicationNumber
+        temp_person.name = ch["name"]
+        temp_person.idNumber = ch["idNumber"]
+        temp_person.phoneNumber = ch["phoneNumber"]
+        temp_person.insert()
+        
     return {"message": "successes"}
 
 # 获取历史记录
 @app.get("/safety/applications/{user_id}")
 async def root(user_id: str):
-    return []
+    res_head = (SafeFormHead
+           .select()
+           .where(SafeFormHead.phoneNumber == user_id)
+        )
+    res_work = (workLocation
+           .select()
+           .join(SafeFormHead, JOIN.LEFT_OUTER, on=SafeFormHead.applicationNumber)
+           .where(SafeFormHead.phoneNumber == user_id)
+        )
+    res_danger = (dangerTypes
+           .select()
+           .join(SafeFormHead, JOIN.LEFT_OUTER, on=SafeFormHead.applicationNumber)
+           .where(SafeFormHead.phoneNumber == user_id)
+        )
+    res_accompaning = (accompaningPersons
+           .select()
+           .join(SafeFormHead, JOIN.LEFT_OUTER, on=SafeFormHead.applicationNumber)
+           .where(SafeFormHead.phoneNumber == user_id)
+        )
+    print(res_head)
+    res = []
+    return res
 
 
 if __name__ == "__main__":
+    create_tables()
+    print("数据库准备完成")
     uvicorn.run("main:app", host="localhost", port=5000, log_level="debug")
--- fastapi_back/model.py
+++ fastapi_back/model.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
-from peewee import PostgresqlDatabase, Model, TextField, IntegerField, SQL
+from peewee import PostgresqlDatabase, Model, TextField, IntegerField, BooleanField, IdentityField, ForeignKeyField
 
 # ------------前后端请求的模型------------
-class SafeFrom(BaseModel):
+class SafeForm(BaseModel):
     applicationNumber: str
     name: str
     idNumber: str
@@ -35,7 +35,7 @@ class SafeFrom(BaseModel):
     accompaningCount:int
     accompaningPersons: list
 
-
+# ------------数据库表的模型------------
 database = PostgresqlDatabase(
     'public',
     host='172.29.22.83',
@@ -43,11 +43,56 @@ database = PostgresqlDatabase(
     user='postgres',
     password='Swq8855830.')
 
-# ------------数据库表的模型------------
-class MyUser (Model):
-    name = TextField()
-    city = TextField(constraints=[SQL("DEFAULT 'Mumbai'")])
-    age = IntegerField()
+class bd_base_model(Model):
     class Meta:
         database = database
-        db_table = 'mysql'
+
+class SafeFormHead(bd_base_model):
+    applicationNumber = TextField(null=False, unique=True)
+    name = TextField(null=False)
+    idNumber = TextField(null=False)
+    companyName = TextField(null=False) 
+    phoneNumber = TextField(null=False)
+
+    submitTime = TextField(null=False)
+    startDate = TextField(null=False)
+    startTime = TextField(null=False)
+    workingHours = TextField(null=False)
+    workType = TextField(null=False)
+
+    isProductWork = BooleanField()
+    projectName = TextField(null=False)
+    vehicleNumber = TextField(null=False)
+    trackPosition = TextField(null=False)
+    
+    workContent = TextField(null=False)
+    workBasis = TextField(null=False)
+    basisNumber = TextField(null=False)
+
+    isDangerousWork = BooleanField()
+
+    notifierName = TextField(null=False)
+    notifierNumber = TextField(null=False)
+    notifierDepartment = TextField(null=False)
+
+    accompaningCount = IntegerField()
+    accompaningPersons: list
+    age = IntegerField()
+    
+class workLocation(bd_base_model):
+    formApplicationNumber = TextField(null=False)
+    workLocation = TextField(null=False)
+    
+class dangerTypes(bd_base_model):
+    formApplicationNumber = TextField(null=False)
+    dangerTypes = TextField(null=False)
+    
+class accompaningPersons(bd_base_model):
+    formApplicationNumber = TextField(null=False)
+    name = TextField(null=False)
+    idNumber = TextField(null=False)
+    phoneNumber = TextField(null=False)
+
+def create_tables():
+    with database:
+        database.create_tables([SafeFormHead, workLocation, dangerTypes, accompaningPersons])
